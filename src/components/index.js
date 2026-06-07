aee00d308d4393ccc0833475bd2f14ace1f4512c
Piyush Foreal <piyush@bipolarentertainment.com>
2026-06-06 21:52:38 -0500

    Slim - Removed Orphaned Files

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-// Components index for easier imports
-export { UploadStep } from './UploadStep.js';
-export { SurveyStep } from './SurveyStep.js';
-export { StyleThemeStep } from './StyleThemeStep.js';
-export { TestingStep } from './TestingStep.js';
-export { ProgressStep } from './ProgressStep.js';
-export { CardGallery } from './CardGallery.js';
-export { ApiSettings } from './ApiSettings.js';
--- src/features/gifts/hero/index.js
+++
@@ -1,6 +0,0 @@
-export { OccasioHero } from './OccasioHero.jsx';
-export { KeepsakeCarousel } from './KeepsakeCarousel.jsx';
-export { SubtleConfetti } from './SubtleConfetti.jsx';
-export { Polaroid } from './Polaroid.jsx';
-export { Wordmark } from './Wordmark.jsx';
-export { DEFAULT_PHOTOS } from './defaultPhotos.js';
